842a7d29f235db2cc36fe0998fa996d22095035c
Vitaliy Gnatenko <vitaliy.gnatenko@apriorit.com>
2023-04-20 15:04:25 +0300

    Add 2.6.cpp

---
+++ cracking_codding_tasks/Second_chapter/2.6.cpp
@@ -0,0 +1,25 @@
+﻿/*
+* Реализуйте функцию, проверяющую, является ли связный список палиндромом. 
+*/
+
+#include "../my_list.h"
+#include <iostream>
+#include <string>
+
+//int main()
+//{
+//    list<int> l{ 1,2,3,4,3,2,1 };
+//
+//    l.print();
+//    if (!l.is_palindrom())
+//    {
+//        std::cout << "List is not palindrom\n";
+//    }
+//    else
+//    {
+//        std::cout << "List is palindrom\n";
+//    }
+//
+//    
+//    return 0;
+//}
